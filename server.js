39506b141a7c8bc25bc1a6a8c8aab75a393c2066
DevanshGupta15 <devanshguptahero@gmail.com>
2025-06-15 22:40:18 +0530

    Added /api/hello endpoint

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello from deployed Node.js server!');
 });
 
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from the new API!' });
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
